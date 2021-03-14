1b59a77c4a1137ca9bc11c2b0c971fe9fb432a5f
jayaramsivaramannair <jayaramsivaramannair@gmail.com>
2021-03-14 12:36:21 -0400

    Replaced ProtectedRoute component with PrivateRoute component

--- friends/src/App.js
+++ friends/src/App.js
@@ -4,7 +4,6 @@ import { Route, Switch } from 'react-router-dom';
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 import PrivateRoute from "./components/PrivateRoute";
-import ProtectedRoute from "./components/ProtectedRoute";
 import FriendDetails from "./components/FriendDetails";
 import AddFriend from "./components/AddFriend";
 
@@ -21,7 +20,7 @@ function App() {
         </Route>
         <PrivateRoute exact path="/dashboard" component={Dashboard} friends={friends} setFriends={setFriends} />
         <PrivateRoute exact path="/dashboard/addFriend" component={AddFriend} />
-        <ProtectedRoute exact path="/dashboard/:friendId" component={FriendDetails} friends={friends} />
+        <PrivateRoute exact path="/dashboard/:friendId" component={FriendDetails} friends={friends} />
         {/*Below acts as the default component when an invalid url is provided*/}
         <Route>
           <Login />
